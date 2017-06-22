154304669ba6c2e7278a8f3d02cd5b5d4035de84
Anukul chand <anukulchand1@gmail.com>
2017-06-22 20:34:34 +0530

    updated to get desired output

--- readingwords2_code.cpp
+++ readingwords2_code.cpp
@@ -5,28 +5,12 @@
 using namespace std;
 int main()
 {
-    ifstream in_file("page_2.txt");
+    
    list<int> line1;
-   int track=0;
     map<string,list<int> > words;
 
     
-  string line;
-   while(getline(in_file, line))
-    {
-         // Got a line
-         stringstream linestream(line);
-          track++;
-         string  tword;
-
-         while(linestream >> tword)
-         {
-            line1.push_back(track);
-             words.insert(make_pair(tword,line1));
-         }
-    
-    }
-     in_file.close();
+  
     
    
        
@@ -35,18 +19,54 @@ int main()
 
          string word;
         while(ofile>> word)
-        {
-              int flag=0;
-           for (map<string,list<int> >::iterator it = words.begin();it != words.end();++it)
+        { 
+               ifstream in_file("page_2.txt");
+               string line;int track=0;
+              while(getline(in_file, line))
+                 {
+         // Got a line
+             stringstream linestream(line);
+               track++;
+             string  tword;
+
+                  while(linestream >> tword)
+                      {   if(tword==word)
+                            {
+                           line1.push_back(track);
+                            } 
+                          
+                      }
+                  
+                }
+               words.insert(make_pair(word,line1));
+              line1.clear();
+              in_file.close();
+       }
+        ofile.close();
+
+
+ifstream ofile1("queries.txt");
+   string w;
+ while(ofile1>>w)
+{
+ list <int> li;
+             li=words[w]; 
+              
+                  if(!li.empty())
                      {
-                                if((it->first).compare(word) == 0)
-                                       {
-                                     cout<<"word : "<<word<<"\n";
-                                        cout<<"line is :"<<it->second.back()<<"\n\n";
-                                            flag=1;break;
-                                         }
-                                   
-                        }
+                           for (std::list<int>::iterator it = li.begin(); it != li.end(); it++)
+                                 {              cout<<"word : "<<w<<"\n"; 
+                                               cout<<"line is :"<< *it <<"\n\n"<<endl;    
+                                  }                      
+
+                           
+                          
+                     } 
+                   else
+                     {
+                         cout<<"word : "<<w<<"\n"; 
+                         cout<<"Not found anywhere!!! \n\n"<<endl;
+                      }
 
                                      /*if(flag==0)
                                      {
@@ -54,8 +74,9 @@ int main()
                                       cout<<"WORD NOT FOUND!!\n\n"<<endl;
                                      }*/
         }
+
     
-            ofile.close();   
+            ofile1.close();   
             
 return 0;
 }
